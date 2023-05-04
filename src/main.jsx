8ec0a9da22447159a08091b50ad0a5432d8f293c
IvanVazquezDL <ivanvazquezdeleon11@gmail.com>
2023-05-03 19:20:27 -0600

    CounterApp eliminado

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import CounterApp from './CounterApp';
 import FiboCounter from './CounterFibo';
 
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <CounterApp value={ 1 }/> */}
         <FiboCounter fiboStart={ 0 }/>
     </React.StrictMode>
 )
